e2356f6c1c203482c8cba7d03194de500eef6c3c
Maksim <maxkov112211@gmail.com>
2021-03-12 01:40:37 +0300

    init:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function replaceAt(string, char, to) {
+    return string.substring(0, char) + to + string.substring(char + 1);
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 
+    let Stack=[];
+  let openElement=0;
+
+  for (let i=0; i<str.length; i++){
+    for (const [x,y] of bracketsConfig){
+      if (str[i] == y) {
+        if (Stack[openElement-1] == x) {
+          Stack.pop();
+          openElement--; 
+        }else {
+          Stack.push(str[i]);
+          openElement++;
+        }
+      }else {
+        if (str[i] == x){
+          Stack.push(str[i]);
+          openElement++;
+        }        
+      }
+    }
+  }
+  return !(Stack.length);
 }
